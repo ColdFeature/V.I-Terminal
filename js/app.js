66bb9493a40f3eab79d5836d6dab8c430289f65a
ColdFeature <93226649+ColdFeature@users.noreply.github.com>
2021-10-28 01:59:20 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ var typeWriter = function (selector, type, interval) {
 
                 all.innerHTML = newText;
 
-                end = end + .9; //loops through the text in the element
+                end = end + .6; //loops through the text in the element
 
                 if (newText === text) {
 
